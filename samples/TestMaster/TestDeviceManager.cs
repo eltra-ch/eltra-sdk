f66237952b6f44105e1fb8832dc50ddf38238b02
Dawid Sienkiewicz <dsienkiewicz@outlook.com>
2020-08-30 16:26:14 +0200

    test master (used by unittests)

---
+++ samples/TestMaster/TestDeviceManager.cs
@@ -0,0 +1,14 @@
+﻿using EltraConnector.Master.Device;
+
+namespace TestMaster
+{
+    class TestDeviceManager : MasterDeviceManager
+    {
+        public TestDeviceManager(string deviceDescriptionFilePath)
+        {
+            AddDevice(new TestDevice(deviceDescriptionFilePath, 1));
+            AddDevice(new TestDevice(deviceDescriptionFilePath, 2));
+            AddDevice(new TestDevice(deviceDescriptionFilePath, 3));
+        }
+    }
+}
