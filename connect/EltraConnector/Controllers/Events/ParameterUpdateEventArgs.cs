8505e8865a79cf8be576e98139eb65927e8be492
Dawid Sienkiewicz <dsienkiewicz@outlook.com>
2020-09-19 12:56:43 +0200

    streema sample #26

---
+++ connect/EltraConnector/Controllers/Events/ParameterUpdateEventArgs.cs
@@ -0,0 +1,21 @@
+﻿using EltraCommon.Contracts.Devices;
+using System;
+
+namespace EltraConnector.Controllers.Events
+{
+    /// <summary>
+    /// ParameterUpdateEventArgs
+    /// </summary>
+    public class ParameterUpdateEventArgs : EventArgs
+    {
+        /// <summary>
+        /// Device
+        /// </summary>
+        public EltraDevice Device { get; set; }
+
+        /// <summary>
+        /// Update Result
+        /// </summary>
+        public bool Result { get; set; }
+    }
+}
