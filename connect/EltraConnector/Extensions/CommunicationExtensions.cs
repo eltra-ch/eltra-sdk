a843f5039e5e4c917114a46edd6e43751aa4e2fb
Dawid Sienkiewicz <dsienkiewicz@outlook.com>
2020-12-29 10:49:22 +0100

    handling controll messages logging (KA, ACK) #60

---
+++ connect/EltraConnector/Extensions/CommunicationExtensions.cs
@@ -0,0 +1,25 @@
+﻿using EltraCommon.Contracts.Ws;
+
+namespace EltraConnector.Extensions
+{
+    internal static class CommunicationExtensions
+    {
+        public static bool IsControlMessage(this WsMessage message)
+        {
+            bool result = false;
+            var data = message.Data;
+
+            if (!string.IsNullOrEmpty(data))
+            {
+                var msg = data.Trim(new char[] { '\"' });
+
+                if (msg == "ACK" || msg == "KEEPALIVE")
+                {
+                    result = true;
+                }
+            }
+
+            return result;
+        }
+    }
+}
